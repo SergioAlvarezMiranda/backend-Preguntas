bb1033deb184222c419cceb46ce5fabdefc933fb
Juanma7882 <juanmaf236@gmail.com>
2025-09-20 19:46:03 -0300

    conexion a la base de datos Co-authored-by: Sergio Luis Alvarez Miranda <SergioAlvarezMiranda@users.noreply.github.com>

---
+++ dbConexion.js
@@ -0,0 +1,18 @@
+//! npm install pg esto permite la  conexión a postgresql
+
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "dbPregunta",
+//   password: "",
+//   port: 5433
+// });
+
+// async function test() {
+//   const res = await pool.query("SELECT NOW()");
+//   console.log(res.rows);
+// }
+// test();
+// module.exports = pool;
