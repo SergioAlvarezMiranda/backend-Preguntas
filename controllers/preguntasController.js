5e95aae06ee0a2cd39e9cc1594aa7ff03d3be470
Sergio <cesio11110@gmail.com>
2025-09-23 23:11:12 -0500

    mejorando la ejecucion del index

--- controllers/preguntasController.js
+++ controllers/preguntasController.js
@@ -1,8 +1,9 @@
+const CardService = require("../services/CardService");
 
-
-const obtenerPreguntas = async (res) => {
+const obtenerPreguntas = async (req, res) => {
     try {
-        
+        const preguntas = await CardService.listar();
+        res.json(preguntas);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Error obteniendo las preguntas" });
@@ -10,3 +11,4 @@ const obtenerPreguntas = async (res) => {
 };
 
 module.exports = { obtenerPreguntas };
+// ...existing code...
--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-const express = require("express");
-const cors = require("cors");
 const preguntasRoutes = require("./routes/preguntas");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 app.use("/api/preguntas", preguntasRoutes);
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+module.exports = { preguntasRoutes };
